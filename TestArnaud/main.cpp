4d7cb7f444e11e21a2998d2ead48289011e3ae6b
Roman PHILIP <romanphlp@gmail.com>
2020-04-26 16:38:34 +0200

    Initial Commit

---
+++ TestArnaud/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestArnaud
+//
+//  Created by Roman Philip on 26/04/2020.
+//  Copyright © 2020 Roman Philip. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
